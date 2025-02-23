6d4876d3270906fb75495086a302aa2f8fc8e149
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-02-23 16:40:33 +0300

    create project and start implementing

---
+++ src/LA/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Type = decimal;
+namespace LA
+{
+    internal class Program
+    {
+        static Type eps = 1e-6M;
+        static Type AbsoluteError(Type x, Type x_star)
+        {
+            return Math.Abs(x - x_star);
+        }
+        static Type RelativeError(Type x, Type x_star)
+        {
+            if (Math.Abs(x) < eps)
+                throw new Exception($"Value too small : {x}");
+            return AbsoluteError(x, x_star) / Math.Abs(x);
+        }
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
